07609785722828dbb9a74ab3b32928b308446b4f
Iuri Araujo Ramos <iuri.ramos@ab-inbev.com>
2021-11-01 15:34:12 -0300

    Formatting footer with maps and size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,63 @@
+import { Box, Container, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GoogleMapsMarker from "./map/GoogleMaps";
+
+const useStyles = makeStyles((theme) => ({
+  general: {
+    marginTop: "2%",
+    marginBottom: "2%",
+    backroundColor: 'yellow'
+  },
+  images: {
+    [theme.breakpoints.down("xs")]: {
+      width: "410px",
+    },
+  },
+  box: {
+    display: "flex",
+    justifyContent: "space-around",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  boxItem: {
+    width: "310px",
+    marginLeft: "10px",
+    marginRight: "10px",
+    [theme.breakpoints.down("xs")]: {
+      width: "550px",
+      marginLeft: "0px",
+      marginRight: "0px",
+      marginTop: "10px",
+      marginBottom: "10px",
+    },
+    background: "#efefef",
+  }
+}));
+
 export default function Footer() {
-  return <div>Footer</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Container className={classes.general}>
+        <Box className={classes.box}>
+          <Paper variant="outlined" className={classes.boxItem}>
+            <Typography variant="body1">Links Utéis</Typography>
+          </Paper>
+          <Paper variant="outlined">
+            <Typography variant="body1">
+              <GoogleMapsMarker />
+            </Typography>
+          </Paper>
+          <Paper variant="outlined" className={classes.boxItem}>
+            <Typography variant="body1">Registro de Imóveis/Títulos e
+              Documentos/Pessoas Juridicas
+              Atendimento: de Segunda a Sexta, das 9 às 16 horas
+              Contato: (11) 4013-9161
+            </Typography>
+          </Paper>
+        </Box>
+      </Container>
+    </div>
+  );
 }
---
+++ src/components/map/GoogleMaps.tsx
@@ -0,0 +1,12 @@
+import Iframe from 'react-iframe'
+
+export default function GoogleMapsMarker() {
+  return (
+    <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.407951812207!2d-47.29761548502776!3d-23.264621984831066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf501b1aa138af%3A0xf28df93ade75cd67!2sCart%C3%B3rio%20de%20Registro%20C%C3%ADvil%20em%20Itu!5e0!3m2!1spt-BR!2sbr!4v1635782075925!5m2!1spt-BR!2sbr"
+      width="550px"
+      height="450px"
+      id="myGooggleMaps"
+      position="relative" />
+
+  )
+}
